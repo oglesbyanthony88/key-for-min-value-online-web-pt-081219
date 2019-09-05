980a8591991f7070921b49745ae7a895f5eb5f45
Anthony Oglesby <ogle1ant@gmail.com>
2019-09-05 15:22:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  hash.each do |a, b|
+    if lowest_value == nil || b < lowest_value
+      lowest_value = b
+      lowest_key = a
     end
   end
   lowest_key
